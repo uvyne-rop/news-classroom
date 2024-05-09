fd63c2796d243f809868be46a21e57360ed84103
uvyne-rop <ropchiri7@gmail.com>
2024-05-09 08:52:51 +0300

    adjusted the code

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,23 +1,3 @@
-// // import React, { useState } from 'react';
-// // // import './MainLayout.css';
-
-// // const MainLayout = () => {
-// //     const [darkMode, setDarkMode] = useState(false);
-
-// //     const toggleDarkMode = () => {
-// //         setDarkMode(!darkMode);
-// //     };
-
-// //     return (
-// //         <div className={darkMode ? "dark" : ""}>
-// //             <button onClick={toggleDarkMode} className="m-4 p-2 bg-gray-300 hover:bg-gray-400 rounded">Toggle Mode</button>
-// //             <h1 className="text-3xl font-bold">{darkMode ? 'Dark Mode' : 'Light Mode'}</h1>
-// //             {/* Your layout components */}
-// //         </div>
-// //     );
-// // };
-
-// // export default MainLayout;
 
 
 // // MainLayout.js
