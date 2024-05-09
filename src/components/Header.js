96cf52ee095f0dcdd706e9bb463aa3715005fafc
uvyne-rop <ropchiri7@gmail.com>
2024-05-09 08:56:50 +0300

    styled the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header({ onDarkModeClick }) {
   return (
     <header>
-      <h2>See The Latest</h2>
+      <h2 className="title text-center text-4xl font-bold py-20 px-30">See The Latest</h2>
       <button onClick={onDarkModeClick}>Dark Mode</button>
     </header>
   );
